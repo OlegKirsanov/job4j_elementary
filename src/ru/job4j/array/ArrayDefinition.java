9fb55561023006ba939f9d87232050d93289b888
OlegKirsanov <spoungebender@yandex.ru>
2020-03-21 12:39:01 +0300

    6.0.2. Заполнение массива.[#258609], добавление в ArrayDefinition массива строк names

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Pyotr Petrov";
+        names[2] = "Fyodor Fyodorov";
+        names[3] = "Denis Denisov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
