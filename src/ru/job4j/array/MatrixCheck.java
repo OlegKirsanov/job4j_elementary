fd62232a91bead791f241d7add90b5ef6bb2a68d
Oleg Kirsanov <spoungebender@yandex.ru>
2020-03-23 00:55:44 +0300

    6.7.2. Моно столбец в матрице.[#258607] , добавил новый метод MonoVertical и тесты к нему.

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -11,4 +11,15 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean monoVertical(char[][] board, int col) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
 }
--- src/ru/job4j/array/MatrixCheckTest.java
+++ src/ru/job4j/array/MatrixCheckTest.java
@@ -26,4 +26,26 @@ public class MatrixCheckTest {
         boolean result = MatrixCheck.monoHorizontal(input, 0);
         assertThat(result, is(false));
     }
+
+    @Test
+    public void whenHasMonoVertical() {
+        char[][] input = {
+                {' ', 'X', ' '},
+                {' ', 'X', ' '},
+                {' ', 'X', ' '}
+        };
+        boolean result = MatrixCheck.monoVertical(input, 1);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotHaveMonoVertical() {
+        char[][] input = {
+                {' ', 'X', ' '},
+                {' ', 'X', ' '},
+                {' ', 'X', ' '}
+        };
+        boolean result = MatrixCheck.monoVertical(input, 0);
+        assertThat(result, is(false));
+    }
 }
