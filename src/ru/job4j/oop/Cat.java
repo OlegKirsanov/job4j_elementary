1dfdefa8b814e9e048a020fcd52e8ed007e43fa2
Oleg Kirsanov <spoungebender@yandex.ru>
2020-04-02 14:24:22 +0300

    1.5. Поля объекта[#258751], создал класс Cat

---
+++ src/ru/job4j/oop/Cat.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name + " eats " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There is Gav's food");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There's Black's food");
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
+    }
+}
