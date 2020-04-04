8145cc1eda2d0249cedd18f9b4b67dfc16d39d65
Oleg Kirsanov <spoungebender@yandex.ru>
2020-04-04 21:02:03 +0300

    2.1. Рефакторинг - Расстояние между точками.[#258746], update Point.java, PointTest.java

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -3,17 +3,23 @@ package ru.job4j.condition;
 import static java.lang.Math.*;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
-        return rsl;
+
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow((this.x - that.x), 2) + pow((this.y - that.y), 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        result = Point.distance(0, 0, 4, 2);
-        System.out.println("result (0, 0) to (4, 4) " + result);
-        result = Point.distance(-1, -1, 6, 4);
-        System.out.println("result (-1, -1) to (6, 4) " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
--- src/ru/job4j/condition/PointTest.java
+++ src/ru/job4j/condition/PointTest.java
@@ -6,12 +6,10 @@ import org.junit.Test;
 public class PointTest {
     @Test
     public void distance() {
-        int inX1 = 0;
-        int inY1 = 0;
-        int inX2 = 4;
-        int inY2 = 2;
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 2);
         double expected = 4.47;
-        double out = Point.distance(inX1, inY1, inX2, inY2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
