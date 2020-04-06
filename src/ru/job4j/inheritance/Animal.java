51c9e56211d098817682465aaf234edcc12b7f09
Oleg Kirsanov <spoungebender@yandex.ru>
2020-04-06 17:11:42 +0300

    2. Перегрузка конструктора.[#258762]

--- src/ru/job4j/inheritance/Animal.java
+++ src/ru/job4j/inheritance/Animal.java
@@ -1,6 +1,12 @@
 package ru.job4j.inheritance;
 
 public class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
     public Animal() {
         System.out.println("Animal");
     }
