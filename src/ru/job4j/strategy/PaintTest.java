616213cfdea76bc3919a98116f290d0f8ec75b77
Oleg Kirsanov <spoungebender@yandex.ru>
2020-04-20 20:23:08 +0300

    9. Написать тесты на StartUI.[#258728]

--- src/ru/job4j/strategy/PaintTest.java
+++ src/ru/job4j/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -10,11 +12,21 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(stdout);
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
                 new String(out.toByteArray()),
@@ -27,14 +39,11 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
+        loadOutput();
         new Paint().draw(new Triangle());
         assertThat(
                 new String(out.toByteArray()),
@@ -47,7 +56,7 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
+        backOutput();
     }
 }
 
